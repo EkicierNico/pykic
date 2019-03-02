2de43cc35c710aff6183aa8848036112cd9da938
Nicolas Ekicier <ekicier.nico@hotmail.fr>
2019-03-02 14:29:25 +0100

    [SYNC] Add copyright file SMAC + changes in miscellaneous

--- miscellaneous/miscdate.py
+++ miscellaneous/miscdate.py
@@ -1,2 +1,27 @@
 import time, datetime
 from datetime import date
+
+"""
+Dates utilities
+Author:     Nicolas EKICIER
+Release:    V1    03/2019
+"""
+
+def dtoday(format='%Y%m%d'):
+    """
+    Return date of today into a string
+    :param format:  format of output string (ex: '%d-%m-%Y' == '23-02-2019')
+    :return:        str date
+    """
+    return strdate
+
+
+def dconvert(datein, fmtin, fmtout):
+    """
+    Convert date format into another
+    :param datein:  input string with date
+    :param fmtin:   input format (ex: '%d-%m-%Y' == '23-02-2019')
+    :param fmtout:  output format (ex: '%Y%m%d' == '20190223')
+    :return:        str date
+    """
+    return strdate
--- miscellaneous/signalproc.py
+++ miscellaneous/signalproc.py
@@ -1,22 +1,44 @@
 import numpy as np
-from scipy.sparse import eye
 
 """
 Signal processing utilities
 Author:     Nicolas EKICIER
-Release:    V01    03/2019
+Release:    V1.01    03/2019
 """
 
-def whittf(y, beta, order):
+def whittf(y, weight=None, beta=1000, order=3):
     """
     Whittaker smoothing
-    :param y:       vector data
-    :param beta:    smoothing parameter > 0
-    :param order:   order of smoothing
-    :return:        smoothed data
+    :param y:       vector data (signal)
+    :param weight:  weights of each sample (one by default)
+    :param beta:    penalization parameter (> 0)
+    :param order:   derivation order
+    :return:        smooth signal
     """
     m = len(y)
-    p = np.diff(eye(m).toarray(), n=order)
-    inter = np.dot(p.transpose(), p)
-    yf = np.linalg.solve((eye(m).toarray() + np.dot(beta, inter)), y)
+    speye = np.eye(m)
+    p = np.diff(speye, n=order)
+
+    if weight is None:
+        diag = speye
+    else:
+        diag = np.diag(weight)
+
+    pp = np.transpose(p)
+    yf = np.linalg.solve(diag + beta*np.dot(p, pp), np.dot(diag, y))
     return yf
+
+
+def outliers(input):
+    """
+    Extract index of outliers in input
+    :param input:   vector data (signal)
+    :return:        index
+    """
+    Quart = np.percentile(input, [25, 75])  # 1er et 3ème quartile
+    IQuart = Quart[1] - Quart[0]    # interquartile
+    wsup = Quart[1] + 1.5 * IQuart  # whisker sup
+    winf = Quart[0] - 1.5 * IQuart  # whisker inf
+
+    idx = np.flatnonzero((input < winf) | (input > wsup))
+    return idx
