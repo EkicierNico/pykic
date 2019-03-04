f45f4fa8994fb90cdb72d01b32342de2f5b491a9
Nicolas Ekicier <ekicier.nico@hotmail.fr>
2019-03-04 22:12:55 +0100

    [EVOL] Add shpbuf function

--- vector/pykic_ogr.py
+++ vector/pykic_ogr.py
@@ -8,32 +8,29 @@ import logging
 """
 OGR utilities
 Author:     Nicolas EKICIER
-Release:    V01    02/2019
+Release:    V1.1    03/2019
+    - Add shpbuf function
+    
+            V1      02/2019
+    - Initialization
 """
 
 def ogreproj (layer, oEPSG):
     """
     Reprojection of an OGR layer
     :param layer:   Geopandas Dataframe OR path of shapefile
-    :param oEPSG:   EPSG value for destination
+    :param oEPSG:   EPSG value for destination (int)
     :return:        Reprojected layer
     """
     if type(layer) is str:
-        # Read data from Geopandas
         layer = gpd.read_file(layer)
 
-    # Get projection from input and print in console
-    iEPSG = layer.crs
+    iEPSG = layer.crs # Get projection from input and print in console
     print('Reprojection from {0:s} to epsg:{1:d}'.format(iEPSG['init'], oEPSG))
 
-    # Let's take a copy of our layer
     data_proj = layer.copy()
-
-    # Reproject the geometries by replacing the values with projected ones
-    data_proj = data_proj.to_crs(epsg=oEPSG)
-
-    # Determine the CRS of the GeoDataFrame
-    data_proj.crs = from_epsg(oEPSG)
+    data_proj = data_proj.to_crs(epsg=oEPSG) # Reproject the geometries by replacing the values with projected ones
+    data_proj.crs = from_epsg(oEPSG) # Determine the CRS of the GeoDataFrame
     return data_proj
 
 
@@ -52,3 +49,32 @@ def sprocessing (layer1, layer2, method):
 
     layerm = gpd.sjoin(layer1, layer2, op=method)
     return layerm
+
+
+def shpbuf(distance, input, output=None, fmtout=None):
+    """
+    Create a buffer from shapefile or Geopandas DataFrame
+    :param distance:    Distance of buffer (in the same crs unit)
+    :param input:       Shapefile path or GeoDataFrame
+    :param output:      Write the output (OPTIONAL)
+    :param fmtout:      Output format (OPTIONAL) --> ('CSV', 'ESRI Shapefile'=default, 'GeoJSON', 'GML', 'GPX', 'MapInfo File')
+                        For complete list : import fiona; fiona.supported_drivers
+    :return:            Buffered layer (GeoDataFrame)
+    """
+    if type(input) is str:
+        layer = gpd.read_file(input)
+    else:   # GeoDataFrame
+        layer = input.copy()
+
+    layerb = gpd.GeoDataFrame(layer.buffer(distance))
+    layerb = layerb.rename(columns={0: 'geometry'}).set_geometry('geometry')
+    layerb.crs = layer.crs
+
+    if output is not None:
+        if type(output) is str and '.shp' in output:
+            layerb.to_file(output)
+        elif type(output) is str and fmtout is not None:
+            layerb.to_file(output, driver=fmtout)
+        else:
+            logging.error('Warning : output is not a string or ".shp" extension is missing')
+    return layerb
