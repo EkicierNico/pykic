ac50eb847ba1f280983e1bcf0077f977641aba05
Nicolas Ekicier <nicolas.ekicier@sirs-fr.com>
2019-09-18 14:21:57 +0200

    [EVOL] Add zonstat & checkproj function

--- vector/pykic_ogr.py
+++ vector/pykic_ogr.py
@@ -1,20 +1,68 @@
-import os, glob
+import os, glob, fiona, logging
 import pandas as pd
 import numpy as np
 import geopandas as gpd
 from fiona.crs import from_epsg
-import logging
+from tqdm import tqdm
+
+import raster.pykic_gdal as rpg
 
 """
 OGR utilities
 Author:     Nicolas EKICIER
-Release:    V1.2    03/2019
-    - Add distm function
-    - Add shpbuf function
-    
-            V1      02/2019
-    - Initialization
+Release:    V1.4    09/2019
 """
+
+def zonstat(inshp, inimg, attribut='id'):
+    """
+    Compute zonal statistics (count) on each polygon of shapefile from image
+    Output will be in image folder with "_statz.csv" extent
+    :param inshp:       path of shapefile
+    :param inimg:       path of image
+    :param attribut:    attribute to use in shapefile table (default = 'id')
+    :return:
+    """
+    try:
+        img, _, _, _ = rpg.gdal2array(inimg)
+        uidi = np.unique(img)
+
+        mask = rpg.makemask(inshp, inimg, attribute=attribut)
+        uid = np.unique(mask)
+
+        output = pd.DataFrame(index = uid, columns = uidi)
+        for idmask in tqdm(uid, desc='Zonal Statistics', total=len(uid)):
+            index = np.flatnonzero(mask == idmask)
+            values = img[np.unravel_index(index, img.shape)]
+
+            uidval, uidvalc = np.unique(values, return_counts=True)
+            for idval, idvalc in zip(uidval, uidvalc):
+                output.at[idmask, idval] = idvalc
+
+        output = output.fillna(value=0)
+        output.to_csv(inimg.replace('.tif', '_statz.csv'))
+        return None
+
+    except:
+        logging.error(' ERROR in compute zonal statistics')
+        return None
+
+def checkproj(layer0, layer1):
+    """
+    Check if projections are the same
+    :param layer0:  path of shapefile 1
+    :param layer1:  path of shapefile 2
+    :return:        booleen
+    """
+    with fiona.open(layer0, 'r') as src0:
+        proj0 = src0.crs['init']
+    with fiona.open(layer1, 'r') as src1:
+        proj1 = src1.crs['init']
+
+    if proj0 is not proj1:
+        return False
+    else:
+        return True
+
 
 def ogreproj(layer, oEPSG):
     """
@@ -38,17 +86,22 @@ def ogreproj(layer, oEPSG):
 def sprocessing(layer1, layer2, method):
     """
     Geometric processing between shapefiles
-    :param layer1:  Geopandas Dataframe 1
-    :param layer2:  Geopandas Dataframe 2
+    :param layer1:  Geopandas Dataframe 1 OR path of shapefile
+    :param layer2:  Geopandas Dataframe 2 OR path of shapefile
     :param method:  "intersects", "within", "contains"
     :return:        Result layer
     """
+    if type(layer1) is str:
+        lay1 = gpd.read_file(layer1)
+    if type(layer2) is str:
+        lay2 = gpd.read_file(layer2)
+
     # Check if projections are same
-    if layer1.crs['init'] is not layer2.crs['init']:
+    if not checkproj(layer1, layer2):
         logging.error('Warning : CRS are not the same')
         return None
 
-    layerm = gpd.sjoin(layer1, layer2, op=method)
+    layerm = gpd.sjoin(lay1, lay2, op=method)
     return layerm
 
 
